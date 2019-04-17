2a3e3d1a08e12dab562d66dca0bbad6827b83668
Maksym Sereda <sereda206@gmail.com>
2019-04-17 10:37:21 +0200

    Initial commit

---
+++ src/main/java/Parser.java
@@ -0,0 +1,17 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Parser {
+    private static Document getPage() throws IOException {
+        String url = "http://www.pogoda.spb.ru/";
+        Document page = Jsoup.parse(new URL(url), 3000);
+        return page;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getPage());
+    }
+}
