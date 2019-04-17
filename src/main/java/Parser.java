b7d292508dbdbf0c2f5bff81d0856f2e1a2bd536
Maksym Sereda <sereda206@gmail.com>
2019-04-17 12:54:31 +0200

    Take date from string

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -5,6 +5,8 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
     private static Document getPage() throws IOException {
@@ -13,13 +15,28 @@ public class Parser {
         return page;
     }
 
-    public static void main(String[] args) throws IOException {
+    private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
+
+    private static String getDateFromString(String stringDate) throws Exception {
+        Matcher matcher = pattern.matcher(stringDate);
+        if (matcher.find()){
+            return matcher.group();
+        }
+        throw new Exception("Can't extract date from string!");
+    }
+
+    public static void main(String[] args) throws Exception {
         Document page = getPage();
         //css query language
         Element tableWeather = page.select("table[class = wt]").first();
         Elements names = tableWeather.select("tr[class = wth]");
         Elements values = tableWeather.select("tr[valign =top]");
-        String date = "";
-        System.out.println("        Weather event       Tempreature         Preasure        Humidity        Wind");
+
+        for (Element name : names) {
+            String dateString = name.select("th[id = dt]").text();
+            String date = getDateFromString(dateString);
+            System.out.println(date + "        Weather event       Tempreature         Preasure        Humidity        Wind");
+        }
+
     }
 }
